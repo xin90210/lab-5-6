dd0cc2111103fca451858f4baadf2c0432508896
Alexey Velychko <xin90210@gmail.com>
2023-01-03 17:56:48 +0200

    init

---
+++ ScalaActorsLb5-6-master/hello-actor.scala
@@ -0,0 +1,21 @@
+import akka.actor.Actor
+import akka.actor.ActorSystem
+import akka.actor.Props
+
+class HelloActor extends Actor {
+  def receive = {
+    case "hello" => println("hello back at you")
+    case _       => println("huh?")
+  }
+}
+
+object Main extends App {
+  val system = ActorSystem("HelloSystem")
+  // default Actor constructor
+  val helloActor = system.actorOf(Props[HelloActor], name = "helloactor")
+  helloActor ! "hello"
+  helloActor ! "bonjour"
+
+  Thread.sleep(1000L)
+  system.shutdown()
+}
